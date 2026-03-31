7b6fd9c1f5749a90c59da099cb0119005f930582
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-03-31 19:37:01 +0200

    fix: remove sonarqube from app module, configure in root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("org.sonarqube") version "5.1.0.4882"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SE2-Macht-Koro_Client")
+        property("sonar.organization", "se2-macht-koro")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "app/src/main")
+        property("sonar.tests", "app/src/test")
+    }
 }
