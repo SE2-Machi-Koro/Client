5805eed7e55ec3ace8214e72b8e3c8187a4556f4
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-04-02 14:51:06 +0200

    ci: migrate SonarCloud analysis to Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
+sonarqube {
+    properties {
+        property("sonar.projectKey", "SE2-Machi-Koro_Client")
+        property("sonar.organization", "se2-machi-koro")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "app/src/main")
+        property("sonar.tests", "app/src/test")
+        property("sonar.java.binaries", "app/build/intermediates/javac/debug/classes")
+    }
+}
