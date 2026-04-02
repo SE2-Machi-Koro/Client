8411998ffc3b0da8a8e7bce641f7cc99e1625360
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-04-02 15:19:44 +0200

    fix: split build and sonar steps, use scan-action again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.sonarqube)
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "SE2-Machi-Koro_Client")
-        property("sonar.organization", "se2-machi-koro")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.sources", "app/src/main")
-        property("sonar.tests", "app/src/test")
-        property("sonar.java.binaries", "app/build/intermediates/javac/debug/classes")
-    }
 }
