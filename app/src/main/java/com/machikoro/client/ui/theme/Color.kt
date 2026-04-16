d41007f7558b64feb72caff632771ef54279a120
Ivona-Goranova <iv.goranova@edu.aau.at>
2026-04-16 17:32:14 +0200

    Add complete color system for UI (base, text, buttons, cards)

--- app/src/main/java/com/machikoro/client/ui/theme/Color.kt
+++ app/src/main/java/com/machikoro/client/ui/theme/Color.kt
@@ -2,10 +2,63 @@ package com.machikoro.client.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Design
+val PrimaryBlueDark = Color(0xFF004E7E)
+val PrimaryBlueLight = Color(0xFFB5E1E5)
+val SecondaryBlueGrey = Color(0xFFC4D3DC)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Background
+val BackgroundDefault = White
+val BackgroundOverlay = Color(0xE5F2FAFF)
+
+// Buttons
+val ButtonBlueDark = PrimaryBlueDark
+val ButtonBlueLight = PrimaryBlueLight
+val ButtonBlueGrey = SecondaryBlueGrey
+
+// Text
+val TextBlueDark = PrimaryBlueDark
+val TextBlueLight= PrimaryBlueLight
+val TextWhite = White
+val TextBlack = Black
+
+// Highlight
+val Highlight = Color(0xB2FFF536)
+
+// Card-Blue
+val CardBlueBackground = PrimaryBlueLight
+val CardBlueText = PrimaryBlueDark
+val CardBlueIcon = PrimaryBlueDark
+
+// Card-Green
+val GreenLight = Color(0xFF88B86E)
+val GreenDark = Color(0xFF306514)
+
+val CardGreenBackground = GreenLight
+val CardGreenText = GreenDark
+val CardGreenIcon = GreenDark
+
+// Card-Red
+val RedLight = Color(0xFFC57F7D)
+val RedDark = Color(0xFF743A38)
+
+val CardRedBackground = RedLight
+val CardRedText = RedDark
+val CardRedIcon = RedDark
+
+// Card-Purple
+val PurpleLight = Color(0xFFA371B7)
+val PurpleDark = Color(0xFF431755)
+
+val CardPurpleBackground = PurpleLight
+val CardPurpleText = PurpleDark
+val CardPurpleIcon = PurpleDark
+
+// Landmark
+val OrangeLight = Color(0xFFD59366)
+val OrangeDark = Color(0xFF7D3A1E)
+
+val LandmarkBackground = OrangeLight
+val LandmarkText = OrangeDark
