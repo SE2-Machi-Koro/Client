7536dc08b66305548d96e494fe4b0f4debb1d444
doniliks <lavrovdanylo@gmail.com>
2026-04-19 00:32:47 +0200

    chore: setup basic layout

--- app/src/main/java/com/machikoro/client/ui/win/WinnerScreen.kt
+++ app/src/main/java/com/machikoro/client/ui/win/WinnerScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +23,52 @@ import com.machikoro.client.ui.theme.ClientTheme
 
 @Composable
 fun WinScreen() {
+    Box(modifier = Modifier.fillMaxSize())
+    {
+        Image(
+            painter = painterResource(id = R.drawable.background_left),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.BottomStart).offset(x = (-20).dp, y = 30.dp),
+            alpha = 0.3f
+        )
+        Image(
+            painter = painterResource(id = R.drawable.background_right),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.BottomEnd).offset(x = 15.dp, y = 30.dp),
+            alpha = 0.3f
+        )
+        Column(
+            modifier = Modifier.fillMaxSize().padding(28.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "WINNER SCREEN",
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
+            ) {
+                Text("Name 1")
+                Text("Name 2")
+                Text("Name 3")
+                Text("Name 4")
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                ) {
+                Button(
+                    onClick = { TODO() }
+                ) {
+                    Text("Finish")
+                }
+                Button(
+                    onClick = { TODO() }
+                ) {
+                    Text("Back to lobby")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
