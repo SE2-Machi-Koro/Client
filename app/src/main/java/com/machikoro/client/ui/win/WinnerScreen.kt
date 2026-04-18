0d8ad8de2d69af51bbb9fbbb6a78c76aa2da5b4f
doniliks <lavrovdanylo@gmail.com>
2026-04-19 00:26:30 +0200

    chore: preview setup

--- app/src/main/java/com/machikoro/client/ui/win/WinnerScreen.kt
+++ app/src/main/java/com/machikoro/client/ui/win/WinnerScreen.kt
@@ -1,4 +1,33 @@
 package com.machikoro.client.ui.win
 
-class WinnerScreen {
+
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.machikoro.client.R
+import com.machikoro.client.ui.theme.ClientTheme
+
+@Composable
+fun WinScreen() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WinScreenPreview() {
+    ClientTheme {
+        WinScreen()
+    }
 }
