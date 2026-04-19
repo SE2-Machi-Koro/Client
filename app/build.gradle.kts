95ae2114ed8078124654eaebed5b2e047c9888c5
doniliks <lavrovdanylo@gmail.com>
2026-04-19 14:02:09 +0200

    feat: functions to create a game card based profile with place, profile icon and nickname for winning screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.okhttp)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/machikoro/client/ui/win/WinnerScreen.kt
+++ app/src/main/java/com/machikoro/client/ui/win/WinnerScreen.kt
@@ -2,24 +2,39 @@ package com.machikoro.client.ui.win
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.machikoro.client.R
+import com.machikoro.client.ui.theme.*
 import com.machikoro.client.ui.theme.ClientTheme
+import com.machikoro.client.ui.theme.PrimaryBlueDark
 
 @Composable
 fun WinScreen() {
@@ -38,21 +53,28 @@ fun WinScreen() {
             alpha = 0.3f
         )
         Column(
-            modifier = Modifier.fillMaxSize().padding(28.dp),
+            modifier = Modifier.fillMaxSize().padding(8.dp),
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = "WINNER SCREEN",
+                style = MaterialTheme.typography.headlineLarge,
+                color = PrimaryBlueDark
+                )
+            val list = listOf<String>("Name 1", "Long name", "1", "HsDHDHsssaaassaasasasassaDH") // for testing
+
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterHorizontally),
+                verticalAlignment = Alignment.CenterVertically
             )
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
-            ) {
-                Text("Name 1")
-                Text("Name 2")
-                Text("Name 3")
-                Text("Name 4")
+            {
+                items(list) {
+                    PlayerProfileCard(it, list.indexOf(it) + 1)
+                }
             }
+
             Row(
                 horizontalArrangement = Arrangement.spacedBy(12.dp),
                 ) {
@@ -70,6 +92,64 @@ fun WinScreen() {
         }
     }
 }
+
+@Composable
+fun PlayerProfileCard(name: String, place: Int) {
+
+            Box(
+                modifier = Modifier
+                    .width(170.dp)
+                    .height(200.dp)
+                    .background(
+                        color = CardPurpleBackground,
+                        shape = RoundedCornerShape(28.dp)
+                    )
+                    .padding(all = 8.dp)
+            ) {
+                    if (place == 1) {
+                        Image(
+                            painter = painterResource(id = R.drawable.lobby_host_icon),
+                            contentDescription = null,
+                            alpha = 0.7f,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .offset(0.dp, (-40).dp)
+                        )
+                    }
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.TopCenter),
+                        text = "$place",
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontSize = 36.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.login_user_icon),
+                        contentDescription = null,
+                        alpha = 0.7f,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(80.dp)
+                            .align(Alignment.Center),
+                    )
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = CardPurpleText,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 12.dp)
+                    )
+                }
+
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
