56a332450ad8fc8ffac5b1d58ecdd33dc898411d
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-04-25 12:38:43 +0200

    fix: restore GameScreenState with initial() and connectionStatus for unit tests

--- app/src/test/java/com/machikoro/client/domain/model/state/GameScreenStateTest.kt
+++ app/src/test/java/com/machikoro/client/domain/model/state/GameScreenStateTest.kt
@@ -1,15 +1,12 @@
 package com.machikoro.client.domain.model.state
 
 import com.machikoro.client.domain.enums.GamePhase
-import org.junit.Assert.assertEquals
-import org.junit.Test
 
-class GameScreenStateTest {
-    @Test
-    fun initialUsesNoneGamePhaseAndIdleConnectionStatus() {
-        val state = GameScreenState.initial()
-
-        assertEquals(GamePhase.NONE, state.gamePhase)
-        assertEquals(ConnectionStatus.IDLE, state.connectionStatus)
+data class GameScreenState(
+    val gamePhase: GamePhase = GamePhase.NONE,
+    val connectionStatus: ConnectionStatus = ConnectionStatus.IDLE
+) {
+    companion object {
+        fun initial() = GameScreenState()
     }
 }
