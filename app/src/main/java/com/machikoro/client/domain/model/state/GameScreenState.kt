86093ab83a79b7492f20f01b94ca27cfc71a4eec
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-04-28 13:53:33 +0200

    fix: add connectionStatus and initial() to GameScreenState

--- app/src/main/java/com/machikoro/client/domain/model/state/GameScreenState.kt
+++ app/src/main/java/com/machikoro/client/domain/model/state/GameScreenState.kt
@@ -3,5 +3,10 @@ package com.machikoro.client.domain.model.state
 import com.machikoro.client.domain.enums.GamePhase
 
 data class GameScreenState(
-    val gamePhase: GamePhase = GamePhase.ROLL_DICE
-)
+    val gamePhase: GamePhase = GamePhase.NONE,
+    val connectionStatus: ConnectionStatus = ConnectionStatus.IDLE
+) {
+    companion object {
+        fun initial() = GameScreenState()
+    }
+}
