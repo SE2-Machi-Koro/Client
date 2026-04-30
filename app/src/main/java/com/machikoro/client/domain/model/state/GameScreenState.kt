60b786ae7cc80f60a4ebfc0fa1291f0a32fb0743
eles17 <lev.starman8@gmail.com>
2026-04-30 15:22:12 +0200

    feat: add player coin state model

--- app/src/main/java/com/machikoro/client/domain/model/state/GameScreenState.kt
+++ app/src/main/java/com/machikoro/client/domain/model/state/GameScreenState.kt
@@ -4,12 +4,14 @@ import com.machikoro.client.domain.enums.GamePhase
 
 data class GameScreenState(
     val gamePhase: GamePhase,
-    val connectionStatus: ConnectionStatus
+    val connectionStatus: ConnectionStatus,
+    val players: List<PlayerCoinState>
 ) {
     companion object {
         fun initial() = GameScreenState(
             gamePhase = GamePhase.NONE,
-            connectionStatus = ConnectionStatus.IDLE
+            connectionStatus = ConnectionStatus.IDLE,
+            players = emptyList()
         )
     }
 }
