bdc921f438a8155c1f10db7cd766a43d83b19e19
doniliks <lavrovdanylo@gmail.com>
2026-05-01 22:15:38 +0200

    feat: add second version of game winner screen with one winner only

---
+++ app/src/main/java/com/machikoro/client/ui/win/WinnerScreenOneWinner.kt
@@ -0,0 +1,19 @@
+package com.machikoro.client.ui.win
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.machikoro.client.ui.theme.ClientTheme
+
+@Composable
+fun WinScreenOneWinner() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WinScreenOnePlayerPreview() {
+    ClientTheme {
+        WinScreenOneWinner()
+    }
+}
+
