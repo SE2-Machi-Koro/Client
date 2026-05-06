f85610d148f873ae516e98765cfc30c95d717445
Ivona-Goranova <iv.goranova@edu.aau.at>
2026-05-06 15:15:50 +0200

    cleanup: remove commented-out code

--- app/src/main/java/com/machikoro/client/domain/model/state/StartScreenState.kt
+++ app/src/main/java/com/machikoro/client/domain/model/state/StartScreenState.kt
@@ -3,20 +3,12 @@ package com.machikoro.client.domain.model.state
 data class StartScreenState(
     val title: String,
     val connectionStatus: ConnectionStatus,
-    /*val lobbyStatus: LobbyStatus,
-    val playerList: List<String> = emptyList(),
-    val maxPlayers: Int = 4,
-    val isHost: Boolean = false,*/
     val loggedInAs: String? = null,
 ) {
     companion object {
         fun placeholder() = StartScreenState(
             title = "Machi Koro Client",
             connectionStatus = ConnectionStatus.IDLE,
-            /*lobbyStatus = LobbyStatus.PLACEHOLDER,
-            playerList = emptyList(),
-            maxPlayers = 4,
-            isHost = false,*/
             loggedInAs = null,
         )
     }
