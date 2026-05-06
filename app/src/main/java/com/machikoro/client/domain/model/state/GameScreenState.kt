8ab924e5bee4b562659baf424adec11a80a670c4
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-05-06 13:46:51 +0200

    feat: add diceResult to GameScreenState

--- app/src/main/java/com/machikoro/client/domain/model/state/GameScreenState.kt
+++ app/src/main/java/com/machikoro/client/domain/model/state/GameScreenState.kt
@@ -1,17 +1,20 @@
 package com.machikoro.client.domain.model.state
 
 import com.machikoro.client.domain.enums.GamePhase
+import com.machikoro.client.network.websocket.DiceRollResult
 
 data class GameScreenState(
     val gamePhase: GamePhase,
     val connectionStatus: ConnectionStatus,
-    val players: List<PlayerCoinState>
+    val players: List<PlayerCoinState>,
+    val diceResult: DiceRollResult? = null
 ) {
     companion object {
         fun initial() = GameScreenState(
             gamePhase = GamePhase.NONE,
             connectionStatus = ConnectionStatus.IDLE,
-            players = emptyList()
+            players = emptyList(),
+            diceResult = null
         )
     }
 }
