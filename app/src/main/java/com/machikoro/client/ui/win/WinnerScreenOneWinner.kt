5eeb77d0dd40a6c4bd3865a64aa358631fa35b29
doniliks <lavrovdanylo@gmail.com>
2026-05-11 13:15:14 +0200

    refactor: add new animation

--- app/src/main/java/com/machikoro/client/ui/win/WinnerScreenOneWinner.kt
+++ app/src/main/java/com/machikoro/client/ui/win/WinnerScreenOneWinner.kt
@@ -1,6 +1,7 @@
 package com.machikoro.client.ui.win
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.Image
@@ -15,10 +16,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,12 +32,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.machikoro.client.R
 import com.machikoro.client.ui.theme.*
 import com.machikoro.client.ui.theme.ClientTheme
@@ -42,156 +52,142 @@ import com.machikoro.client.ui.theme.PrimaryBlueDark
 import kotlinx.coroutines.delay
 
 @Composable
-fun WinScreenOneWinner() {
+fun WinScreenOneWinner(winnerName: String, roundsNumber: Int) {
     Box(modifier = Modifier.fillMaxSize())
     {
-        //Background
         Image(
-            painter = painterResource(id = R.drawable.background_left),
+            painter = painterResource(id = R.drawable.frame),
             contentDescription = null,
-            modifier = Modifier.align(Alignment.BottomStart).offset(x = (-20).dp, y = 30.dp),
-            alpha = 0.3f
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
-        Image(
-            painter = painterResource(id = R.drawable.background_right),
-            contentDescription = null,
-            modifier = Modifier.align(Alignment.BottomEnd).offset(x = 15.dp, y = 30.dp),
-            alpha = 0.3f
-        )
-
         //Content
         Column(
             modifier = Modifier.fillMaxSize().padding(all = 12.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            //Headline
-            Text(
-                text = "WINNER SCREEN",
-                style = MaterialTheme.typography.headlineLarge,
-                color = PrimaryBlueDark
-            )
-            val list = listOf<String>("Name 1", "Long name", "1", "HsDHDHsssaaassaasasasassaDH") // for testing
 
-
-            //Player profiles
+            Header("Congratulations to...")
+            //Player cards
             Row(
                 modifier = Modifier.fillMaxWidth()
                     .weight(1f),
-                horizontalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.spacedBy(28.dp, Alignment.CenterHorizontally),
                 verticalAlignment = Alignment.CenterVertically,
             )
             {
-                list.forEachIndexed { i, player ->
-                    var visible by remember { mutableStateOf(false) }
-
-                    LaunchedEffect(Unit) {
-                        delay(300 + i * 500L)
-                        visible = true
-                    }
-
-                    AnimatedVisibility(
-                        visible = visible,
-                        enter = fadeIn() + scaleIn(
-                            initialScale = 0.9f
-                        )
+                AnimatedItem(delayMillis = 1000, animationType = AnimationType.Bounce) {
+                    PlayerProfileCard(winnerName, 1)
+                }
+                AnimatedItem(delayMillis = 2000, animationType = AnimationType.Fade) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        PlayerProfileCard(player, i + 1)
+                        Text(
+                            text = "won the game in \n$roundsNumber rounds!",
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                            maxLines = 2,
+                            textAlign = TextAlign.Center,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                 }
             }
 
-
-
-
-
-            //Buttons
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-            ) {
-                Button(
-                    onClick = { TODO() },
-                    modifier = Modifier.wrapContentSize()
+            AnimatedItem(delayMillis = 5000, animationType = AnimationType.SlideUp) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    Text(text = "Finish game",
-                        style = MaterialTheme.typography.labelLarge,
-                        fontSize = 24.sp,
-                    )
-                }
-                Button(
-                    onClick = { TODO() },
-                    modifier = Modifier.wrapContentSize()
-
-                ) {
-                    Text(text = "Back to current lobby",
-                        style = MaterialTheme.typography.labelLarge,
-                        fontSize = 24.sp,
-                    )
+                    ActionButton("Back to home screen", null)
+                    ActionButton("Play again", null)
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun SmallPlayerProfileCard(name: String, index: Int) {
-    val backgroundColors = listOf(
-        CardPurpleBackground,
-        CardRedBackground,
-        CardGreenBackground,
-        CardBlueBackground
+fun AnimatedItem(
+    delayMillis: Int,
+    animationType: AnimationType = AnimationType.Fade,
+    content: @Composable () -> Unit
+) {
+
+    var visible by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(Unit) {
+        delay(delayMillis.toLong())
+        visible = true
+    }
+
+    val alpha by animateFloatAsState(
+        targetValue = if (visible) 1f else 0f,
+        label = ""
     )
 
-    val textColors = listOf(
-        CardPurpleText,
-        CardRedText,
-        CardGreenText,
-        CardBlueText
+    val scale by animateFloatAsState(
+        targetValue =
+            when (animationType) {
+                AnimationType.Scale,
+                AnimationType.Bounce -> if (visible) 1f else 0.0f
+
+                else -> 1f
+            },
+        label = ""
     )
+
+    val translationY by animateFloatAsState(
+        targetValue =
+            when (animationType) {
+                AnimationType.SlideUp -> if (visible) 0f else 300f
+                AnimationType.Bounce -> if (visible) 0f else 500f
+                else -> 0f
+            },
+        label = ""
+    )
+
 
     Box(
-        modifier = Modifier
-            .width(300.dp)
-            .height(100.dp)
-            .padding(top = 14.dp)
-        ,
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .border(
-                    width = 1.5f.dp,
-                    color = textColors[index - 1],
-                    shape = RoundedCornerShape(28.dp)
-                )
-                .background(
-                    color = backgroundColors[index - 1],
-                    shape = RoundedCornerShape(28.dp)
-                )
-                .padding(8.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        modifier = Modifier.graphicsLayer {
 
-            Text(
-                text = name,
-                color = textColors[index - 1],
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
+            this.alpha =
+                if (animationType == AnimationType.Fade ||
+                    visible
+                ) alpha else 1f
 
+            scaleX = scale
+            scaleY = scale
+
+            this.translationY = translationY
+            this.translationX = translationX
         }
+    ) {
+        content()
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun WinScreenOnePlayerPreview() {
-    ClientTheme {
-        WinScreenOneWinner()
+
+    @Preview(showBackground = true)
+    @Composable
+    fun WinScreenOnePlayerPreview() {
+        ClientTheme {
+            WinScreenOneWinner(winnerName = "Alice", roundsNumber = 5)
+        }
     }
+
+enum class AnimationType {
+    Fade,
+    Scale,
+    SlideUp,
+    Bounce
 }
 
