87eb7f9113a75e37a7158c101b4144a20e2d3919
doniliks <lavrovdanylo@gmail.com>
2026-05-21 16:14:47 +0200

    feat: adjust crown animation

--- app/src/main/java/com/machikoro/client/ui/win/WinnerScreenOneWinner.kt
+++ app/src/main/java/com/machikoro/client/ui/win/WinnerScreenOneWinner.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,54 +41,79 @@ for visibility.
 fun GameOverOneWinner(winnerName: String, roundsNumber: Int) {
     Box(modifier = Modifier.fillMaxSize())
     {
-      Background(R.drawable.game_end)
+        Background(R.drawable.game_end)
         //Content
         Column(
-            modifier = Modifier.fillMaxSize().padding(all = 12.dp),
+            modifier = Modifier
+                .fillMaxSize().padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
             Header("Congratulations to...")
+
+            Spacer(modifier = Modifier.padding(17.dp))
+
             Row(
-                modifier = Modifier.fillMaxWidth()
-                    .weight(1f),
-                horizontalArrangement = Arrangement.spacedBy(40.dp, Alignment.CenterHorizontally),
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(
+                    40.dp,
+                    Alignment.CenterHorizontally
+                ),
                 verticalAlignment = Alignment.CenterVertically,
-            )
-            {
-                //Player card
-                AnimatedItem(delayMillis = 500, animationType = AnimationType.Bounce) {
-                    PlayerProfileCard(winnerName, 1)
-                }
-                AnimatedItem(delayMillis = 1000, animationType = AnimationType.Fade) {
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-                        horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Box(
+                    contentAlignment = Alignment.TopCenter
+                ) {
+
+                    AnimatedItem(
+                        delayMillis = 500,
+                        animationType = AnimationType.Bounce
                     ) {
-                        //TODO: later add more info here like score, coins, etc
-                        RegularInfoText("won the game in \n$roundsNumber rounds!")
+                        PlayerProfileCard(winnerName, 1)
                     }
+                        AnimatedItem(
+                            delayMillis = 1000, // show crown after all cards are shown
+                            animationType = AnimationType.Bounce
+                        ) {
+                            Box(
+                                modifier = Modifier.offset(y = (-28).dp)
+                            ) {
+                                Crown()
+                            }
+                        }
+                }
+
+                AnimatedItem(
+                    delayMillis = 2000,
+                    animationType = AnimationType.Fade
+                ) {
+                    RegularInfoText(
+                        "won the game in \n$roundsNumber rounds!"
+                    )
                 }
             }
 
-            AnimatedItem(delayMillis = 5000, animationType = AnimationType.SlideUp) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    ActionButton("Back to home screen", null)
-                }
+            Spacer(modifier = Modifier.weight(1f))
+
+            AnimatedItem(
+                delayMillis = 5000,
+                animationType = AnimationType.SlideUp
+            ) {
+                ActionButton("Back to home screen", null)
             }
         }
     }
 }
 
 
-    @Preview(showBackground = true)
-    @Composable
-    fun GameOverOnePlayerPreview() {
-        ClientTheme {
-            // Dummy data for preview
-            GameOverOneWinner(winnerName = "Alice", roundsNumber = 5)
-        }
+@Preview(showBackground = true)
+@Composable
+fun GameOverOnePlayerPreview() {
+    ClientTheme {
+        // Dummy data for preview
+        GameOverOneWinner(winnerName = "Alice", roundsNumber = 5)
     }
+}
 
 
