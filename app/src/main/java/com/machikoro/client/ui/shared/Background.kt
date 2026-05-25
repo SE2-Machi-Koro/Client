158169098e29bec87100ed7a7d1571d40bc5d0f6
doniliks <lavrovdanylo@gmail.com>
2026-05-25 22:28:38 +0200

    feat: add other draft background

--- app/src/main/java/com/machikoro/client/ui/shared/Background.kt
+++ app/src/main/java/com/machikoro/client/ui/shared/Background.kt
@@ -1,10 +1,12 @@
 package com.machikoro.client.ui.shared
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +26,6 @@ import com.machikoro.client.R
 @Composable
 fun Background(
     painterResource: Int = R.drawable.background_wood,
-    alpha: Float = 0.1f
 ) {
     Box(
         modifier = Modifier.fillMaxSize()
@@ -37,8 +38,6 @@ fun Background(
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
         )
-
-
     }
 }
 
