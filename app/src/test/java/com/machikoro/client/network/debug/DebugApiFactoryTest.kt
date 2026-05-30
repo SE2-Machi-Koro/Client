01358d434986675b4327b1485055ab66d7381017
Moritz2k1 <moritz.gutschi@outlook.com>
2026-05-30 22:59:51 +0200

    fix: Update tests

--- app/src/test/java/com/machikoro/client/network/debug/DebugApiFactoryTest.kt
+++ app/src/test/java/com/machikoro/client/network/debug/DebugApiFactoryTest.kt
@@ -6,13 +6,20 @@ import org.junit.Test
 class DebugApiFactoryTest {
     @Test
     fun createReturnsDebugApi() {
-        val api = DebugApiFactory.create("http://localhost/")
+        val api = DebugApiFactory.create("http://localhost/") { null }
         assertNotNull(api)
     }
 
     @Test
     fun createWithTrailingSlashSucceeds() {
-        val api = DebugApiFactory.create("http://10.0.2.2:8080/")
+        val api = DebugApiFactory.create("http://10.0.2.2:8080/") { null }
+        assertNotNull(api)
+    }
+
+    @Test
+    fun createWithTokenProviderSucceeds() {
+        // Token provider returning a value should not break construction
+        val api = DebugApiFactory.create("http://localhost/") { "test-token" }
         assertNotNull(api)
     }
 }
