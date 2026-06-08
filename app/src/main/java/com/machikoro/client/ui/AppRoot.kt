bd24b4d7552a4d718a86c7a21e35d73a50c061a2
Lea Kutschera <leavalentinakutschera@gmail.com>
2026-06-08 14:16:52 +0200

    revert: remove rankedPlayers from AppRoot Winner composable call

--- app/src/main/java/com/machikoro/client/ui/AppRoot.kt
+++ app/src/main/java/com/machikoro/client/ui/AppRoot.kt
@@ -40,7 +40,6 @@ import com.machikoro.client.ui.leaderboard.LeaderboardState
 import com.machikoro.client.ui.start.StartScreen
 import com.machikoro.client.ui.theme.ClientTheme
 import com.machikoro.client.ui.win.GameOverOneWinner
-import com.machikoro.client.ui.win.resolveRankedPlayers
 import com.machikoro.client.ui.win.resolveWinnerName
 import kotlinx.coroutines.flow.collectLatest
 
@@ -242,7 +241,6 @@ fun AppRoot(
                 GameOverOneWinner(
                     winnerName = resolveWinnerName(gameScreenState),
                     roundsNumber = gameScreenState.roundNumber ?: 0,
-                    rankedPlayers = resolveRankedPlayers(gameScreenState),
                     onBackHome = onBackHome,
                     onViewLeaderboard = {
                         navController.navigate(AppRoute.Leaderboard.route) {
