397c0eff2ac3ebe25a0fe263f067cdfbe2f0ac47
Ivona-Goranova <ivgoranova@edu.aau.at>
2026-06-14 14:02:50 +0200

    fix: logout and leave lobby button

--- app/src/main/java/com/machikoro/client/ui/lobby/LobbyScreen.kt
+++ app/src/main/java/com/machikoro/client/ui/lobby/LobbyScreen.kt
@@ -253,7 +253,7 @@ fun LobbyScreen(
         }
 
         ArrowTextButton(
-            label = "Logout",
+            label = "Leave Lobby",
             onClick = onLeaveLobby,
             modifier = Modifier.offset(x = 30.dp, y = 28.dp),
             fontSize = 18.sp
