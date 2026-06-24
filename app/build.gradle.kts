a65ea3aa20a142069a8a50ab1027668f439a43cb
Ivona-Goranova <ivgoranova@edu.aau.at>
2026-06-24 14:22:17 +0200

    Remove duplicate foundation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,7 +105,6 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.foundation)
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.androidx.datastore.preferences.core)
